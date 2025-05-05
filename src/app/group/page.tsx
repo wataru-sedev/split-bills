ec7456be671f20e6e9a13677e836ebdc0e8c661a
wataru <kabutomushi2003@gmail.com>
2025-05-05 16:59:32 +0900

    スタイルの調整

--- src/app/group/page.tsx
+++ src/app/group/page.tsx
@@ -2,11 +2,13 @@ import Link from "next/link"
 
 export default function GroupPage () {
   return(
-    <div className="flex flex-col gap-4">
-      <h1>グループページ</h1>
-      <h1>北海道旅行</h1>
-      <p>ドラえもん・のびた・すねお・ジャイアン</p>
-      <Link href='/group/new' className="bg-cyan-500 text-white rounded-xl px-4 py-2 w-40 hover:shadow-lg" >立替記録を追加</Link>
+    <div className="flex justify-center pt-10" >
+      <div className="w-96 bg-white shadow-md rounded-xl p-6 flex flex-col gap-6 items-center">
+        <h1 className="mb-1" >グループページ</h1>
+        <h1 className="mb-1" >北海道旅行</h1>
+        <p>ドラえもん・のびた・すねお・ジャイアン</p>
+        <Link href='/group/new' className="w-full bg-cyan-500 text-white rounded px-4 py-2 hover:shadow-lg" >立替記録を追加</Link>
+      </div>
     </div>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,20 +2,20 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center gap-3">
-      <div className="flex flex-col items-center justify-center gap-4 pt-6">
-        <div className="flex flex-col items-center">
-          <p>グループ名</p>
-          <input type="text" placeholder="北海道旅行" className="border rounded  p-1 hover:shadow-lg"/>
+    <div className="flex justify-center pt-10">
+      <div className="w-96 bg-white shadow-md rounded-xl p-6 flex flex-col gap-6 items-center">
+        <div className="w-full">
+          <p className="text-sm text-gray-600 mb-1" >グループ名</p>
+          <input type="text" placeholder="北海道旅行" className="w-full border rounded px-3 py-2 hover:shadow focus:outline-none focus:ring-2 focus:ring-cyan-300"/>
         </div>
-        <div className="">
-          <p>メンバー名</p>
-          <div>
-            <input type="text" placeholder="たろう" className="border rounded  p-1 hover:shadow-lg" />
+        <div className="w-full">
+          <p className="text-sm text-gray-600 mb-1" >メンバー名</p>
+          <div className="flex gap-2" >
+            <input type="text" placeholder="たろう" className="flex-1 border rounded px-3 py-2 hover:shadow focus:outline-none focus:ring-2 focus:ring-cyan-300" />
             <button className="bg-cyan-500 text-white rounded-xl px-4 py-2 hover:cursor-pointer hover:shadow-lg">追加</button>
           </div>   
         </div>
-        <Link href='/group' className="bg-cyan-500 text-white rounded-xl px-4 py-2 hover:cursor-pointer hover:shadow-lg" >グループページを作成</Link>
+        <Link href='/group' className="w-full bg-cyan-500 text-white rounded px-4 py-2 hover:cursor-pointer hover:shadow-lg" >グループページを作成</Link>
       </div>
     </div>
   );
